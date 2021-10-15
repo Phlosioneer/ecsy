a4aaac7efa9154f9ce621fdeb647a702bac6b24a
Phlosioneer <mattmdrr2@gmail.com>
2021-10-15 02:34:03 -0400

    Update library exports

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ export { System } from "./System.js";
 export { Component } from "./Component.js";
 export { SystemStateComponent } from "./SystemStateComponent.js";
 export { ObjectPool } from "./ObjectPool.js";
-export { Not } from "./Filter.js";
+export { Not, Filter } from "./Filter.js";
+export { Query } from "./Query.js";
+export { QueryEvents, EntityEvents } from "./constants.js";
 export {
   Types,
   createType,
@@ -17,4 +19,5 @@ export {
   cloneClonable,
 } from "./Types.js";
 export { Version } from "./Version.js";
-export { Entity as _Entity } from "./Entity.js";
+export { Entity } from "./Entity.js";
+export { Tag } from "./Tag.js";
